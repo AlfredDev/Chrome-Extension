9dbd93068e433e52a1e59d84ddff831108bfdba6
jose alfredo garcia <dayra14100@gmail.com>
2023-03-20 18:55:20 -0600

    AHora la funcion que convierte a negritas la pagina, funciona presionando un boton del popup

---
+++ popup.js
@@ -0,0 +1,21 @@
+
+
+let fastBtn = document.getElementById('fast');
+
+let fastBtnsss = document.getElementById('adios');
+
+fastBtn.addEventListener('click', async () => {
+
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    // function: bionicRead,
+    files : ['readable_text.js']
+  });
+});
+
+
+fastBtnsss.addEventListener('click', async () => {
+  alert('Remo SOlo')
+})
--- readable_text.js
+++ readable_text.js
@@ -1,77 +1,77 @@
-javascript: (() => {
+(() => {
   //Insercion del nodo
-    const insertAfter = (newNode, existingNode) => {
-      if (existingNode.nextSibling != undefined)
-        existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
-      else
-        existingNode.parentNode.appendChild(newNode);
-    };
-  
-    const HalfBold = (parentElement) => {
-      for (var i = 0; parentElement.childNodes[i] != undefined; i++) {
-        if (parentElement.childNodes[i].nodeName == "#text" &&
-          parentElement.childNodes[i].textContent.trim().length != 0
-        ) {
-          var recentNode = parentElement.childNodes[i];
-          var newNodeCount = 0;
-          parentElement.childNodes[i].textContent.split(/(\s+|\S+)/).forEach(
-            word => {
-              if (word.length == 0) return;
-              var trimmedWordLength = word.trim().length;
-              if (trimmedWordLength == 0) {
-                var textNode = document.createTextNode(word);
-                insertAfter(textNode, recentNode);
-                newNodeCount++;
-                recentNode = textNode;
-                return;
-              }
-  
-              var length = Math.floor(trimmedWordLength / 2);
-              if (length == 0) length = 1;
-  
-              const bold = document.createElement('b');
-              bold.innerHTML = word.slice(0, length);
-              insertAfter(bold, recentNode);
-              newNodeCount++;
-              recentNode = bold;
-  
-              if (word.length == 1) return;
-              var textNode = document.createTextNode(word.slice(length));
+  const insertAfter = (newNode, existingNode) => {
+    if (existingNode.nextSibling != undefined)
+      existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+    else
+      existingNode.parentNode.appendChild(newNode);
+  };
+
+  const HalfBold = (parentElement) => {
+    for (var i = 0; parentElement.childNodes[i] != undefined; i++) {
+      if (parentElement.childNodes[i].nodeName == "#text" &&
+        parentElement.childNodes[i].textContent.trim().length != 0
+      ) {
+        var recentNode = parentElement.childNodes[i];
+        var newNodeCount = 0;
+        parentElement.childNodes[i].textContent.split(/(\s+|\S+)/).forEach(
+          word => {
+            if (word.length == 0) return;
+            var trimmedWordLength = word.trim().length;
+            if (trimmedWordLength == 0) {
+              var textNode = document.createTextNode(word);
               insertAfter(textNode, recentNode);
               newNodeCount++;
               recentNode = textNode;
-            });
-          parentElement.removeChild(parentElement.childNodes[i]);
-          i += newNodeCount - 1;
-        }
+              return;
+            }
+
+            var length = Math.floor(trimmedWordLength / 2);
+            if (length == 0) length = 1;
+
+            const bold = document.createElement('b');
+            bold.innerHTML = word.slice(0, length);
+            insertAfter(bold, recentNode);
+            newNodeCount++;
+            recentNode = bold;
+
+            if (word.length == 1) return;
+            var textNode = document.createTextNode(word.slice(length));
+            insertAfter(textNode, recentNode);
+            newNodeCount++;
+            recentNode = textNode;
+          });
+        parentElement.removeChild(parentElement.childNodes[i]);
+        i += newNodeCount - 1;
       }
-    };
-  
-    var ignoreTags = {
-      B: true,
-      META: true,
-      LINK: true,
-      SCRIPT: true,
-      STYLE: true,
-    };
-  
-    const processDocumentBody = (element) => {
-      if (element == null) return;
-      if (element.body == undefined) return;
-      var collection = element.body.getElementsByTagName("*");
-  
-      for (var i = 0; collection[i] != undefined; i++) {
-        if (ignoreTags[collection[i].nodeName]) continue;
-        if (collection[i].nodeType != 1) continue;
-        if (collection[i].nodeName == "IFRAME") {
-          processDocumentBody(collection[i].contentDocument);
-        } else {
-          if (collection[i].childNodes.length == 0) continue;
-          HalfBold(collection[i]);
-        }
+    }
+  };
+
+  var ignoreTags = {
+    B: true,
+    META: true,
+    LINK: true,
+    SCRIPT: true,
+    STYLE: true,
+  };
+
+  const processDocumentBody = (element) => {
+    if (element == null) return;
+    if (element.body == undefined) return;
+    var collection = element.body.getElementsByTagName("*");
+
+    for (var i = 0; collection[i] != undefined; i++) {
+      if (ignoreTags[collection[i].nodeName]) continue;
+      if (collection[i].nodeType != 1) continue;
+      if (collection[i].nodeName == "IFRAME") {
+        processDocumentBody(collection[i].contentDocument);
+      } else {
+        if (collection[i].childNodes.length == 0) continue;
+        HalfBold(collection[i]);
       }
-    };
-  
-    processDocumentBody(document);
-  
-  })();
+    }
+  };
+
+  processDocumentBody(document);
+
+})();
