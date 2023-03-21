01c3c2c7b6db4f3b74b1111aeba538d505e5bc23
jose alfredo garcia <dayra14100@gmail.com>
2023-03-20 19:06:59 -0600

    AHora la funcion que convierte a negritas la pagina, funciona presionando un boton del popup

--- popup.js
+++ popup.js
@@ -1,17 +1,12 @@
-
-
 let fastBtn = document.getElementById('fast');
-
 let fastBtnsss = document.getElementById('adios');
 
+
 fastBtn.addEventListener('click', async () => {
-
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    // function: bionicRead,
-    files : ['readable_text.js']
+    files: ['/Scripts/readable_text.js']
   });
 });
 
