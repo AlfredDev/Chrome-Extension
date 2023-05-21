6dd28269e79b7914db6808902714309b37605bae
Eliu Palacios <3liu.dev@gmail.com>
2023-05-21 16:28:21 -0600

    Ya funciona el popup

--- content.js
+++ content.js
@@ -1,69 +1,88 @@
-// function getSelectedText() {
-//     if (window.getSelection) {
-//       return window.getSelection().toString();
-//     } else if (document.selection && document.selection.type != 'Control') {
-//       return document.selection.createRange().text;
-//     }
-//     return '';
-//   }
-
-//   function highlightSelection() {
-//     const selectedText = getSelectedText();
-//     const range = window.getSelection().getRangeAt(0);
-//     const span = document.createElement('span');
-//     span.style.backgroundColor = 'yellow';
-//     range.surroundContents(span);
-//   }
-
-//   chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//     if (request.action === 'highlight') {
-//       highlightSelection();
-//     }
-//   });
-function createButton() {
-    const button = document.createElement('button');
-    button.className = 'highlight-button';
-    button.innerHTML = '🧹';
-    document.body.appendChild(button);
-    button.addEventListener('click', highlightSelection);
+function createPopup() {
+  const popup = document.createElement('div');
+  popup.id = 'popup';
+  popup.className = 'popup';
+  
+  const button1 = document.createElement('button');
+  button1.innerHTML = '🟡';
+  button1.addEventListener('click', function() {
+    highlightSelection('yellow');
+    removePopup();
+  });
+  
+  const button2 = document.createElement('button');
+  button2.innerHTML = '🔵';
+  button2.addEventListener('click', function() {
+    highlightSelection('blue');
+    removePopup();
+  });
+  
+  const button3 = document.createElement('button');
+  button3.innerHTML = '🟢';
+  button3.addEventListener('click', function() {
+    highlightSelection('green');
+    removePopup();
+  });
+  
+  popup.appendChild(button1);
+  popup.appendChild(button2);
+  popup.appendChild(button3);
+  
+  document.body.appendChild(popup);
 }
 
-function removeButton() {
-    const button = document.querySelector('.highlight-button');
-    if (button) {
-        button.remove();
+function removePopup() {
+    const popup = document.querySelector('.popup');
+    if (popup) {
+        popup.remove();
+        
     }
+}
+
+
+
+function showPopup(x, y) {
+  const popup = document.getElementById('popup');
+  popup.style.top = y + 'px';
+  popup.style.left = x + 'px';
+  popup.style.position = 'absolute';
+  popup.style.display = 'block';
+}
+
+function hidePopup() {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'none';
 }
 
 function getSelectedText() {
-    if (window.getSelection) {
-        return window.getSelection().toString();
-    } else if (document.selection && document.selection.type != 'Control') {
-        return document.selection.createRange().text;
-    }
-    return '';
+  if (window.getSelection) {
+    return window.getSelection().toString();
+  } else if (document.selection && document.selection.type !== 'Control') {
+    return document.selection.createRange().text;
+  }
+  return '';
 }
 
-function highlightSelection() {
-    const selectedText = getSelectedText();
-    if (selectedText) {
-        const range = window.getSelection().getRangeAt(0);
-        const span = document.createElement('span');
-        span.style.backgroundColor = 'yellow';
-        range.surroundContents(span);
-        removeButton();
-    }
+function highlightSelection(color) {
+  const selectedText = getSelectedText();
+  if (selectedText) {
+    const range = window.getSelection().getRangeAt(0);
+    const span = document.createElement('span');
+    span.style.backgroundColor = color;
+    range.surroundContents(span);
+    window.getSelection().removeAllRanges();
+    removePopup();
+  }
 }
 
 document.addEventListener('mouseup', function (event) {
-    const selectedText = getSelectedText();
-    if (selectedText) {
-        createButton();
-        const button = document.querySelector('.highlight-button');
-        button.style.top = event.pageY + 'px';
-        button.style.left = event.pageX + 'px';
-    } else {
-        removeButton();
-    }
+  const selectedText = getSelectedText();
+  if (selectedText) {
+    createPopup();
+    showPopup(event.pageX, event.pageY);
+  } else {
+    removePopup();
+  }
 });
+
 
