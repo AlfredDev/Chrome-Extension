5f43698ebbe4e6715072974d4ce7a7b001ddd685
Eliu Palacios <3liu.dev@gmail.com>
2023-05-29 12:40:13 -0600

    ya

--- content.js
+++ content.js
@@ -146,3 +146,39 @@ function openNotePopup() {
   popup.appendChild(txt);
   document.body.appendChild(popup);
 }
+
+
+
+// Recibe mensajes de la extensión y realiza las acciones correspondientes
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.command === 'play') {
+        var selectedText = window.getSelection().toString();
+        if (selectedText.length > 0) {
+            speak(selectedText);
+            sendResponse({ result: 'success' });
+        } else {
+            sendResponse({ result: 'error', message: 'No se ha seleccionado texto.' });
+        }
+    } else if (request.command === 'pause') {
+        pause();
+        sendResponse({ result: 'success' });
+    } else if (request.command === 'stop') {
+        pause();
+        stop();
+        sendResponse({ result: 'success' });
+    }
+});
+
+// Utiliza la API de SpeechSynthesis para leer el texto en voz alta
+function speak(text) {
+    var utterance = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(utterance);
+}
+
+function pause() {
+    window.speechSynthesis.pause();
+}
+
+function stop() {
+    window.speechSynthesis.cancel();
+}
